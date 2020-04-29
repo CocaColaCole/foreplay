c40805b67446198bcebcbd0abf5867a718ca8abb
Dane Johnson <dane_johnson@ymail.com>
2020-04-28 19:44:58 -0500

    No more fucking tabs

--- main.lua
+++ main.lua
@@ -19,14 +19,14 @@ local nextId = 1
 local dice1
 local dice2
 local hexLocation = {{300,150},{250,225},{200,300},{250,375},{300,450},{400,450},{500,450},{550,375},{600,300},{550,225},{500,150},{400,150},
-                     {350,225},{300,300},{350,375},{450,375},{500,300},{450,225},{400,300}} --spiral layout
+   {350,225},{300,300},{350,375},{450,375},{500,300},{450,225},{400,300}} --spiral layout
 local harborLocation = {{250,75},{200,150},{150,225},{100,300},{150,375},{200,450},{250,525},{350,525},{450,525},{550,525},
-                        {600,450},{650,375},{700,300},{650,225},{600,150},{550,75},{450,75},{350,75}}
+   {600,450},{650,375},{700,300},{650,225},{600,150},{550,75},{450,75},{350,75}}
 --[[                    {{300,150},{400,150},{500,150},
-                {250,225},{350,225},{450,225},{550,225},
-            {200,300},{300,300},{400,300},{500,300},{600,300},
-                {250,375},{350,375},{450,375},{550,375},
-                    {300,450},{400,450},{500,450}}--x,y --]]
+   {250,225},{350,225},{450,225},{550,225},
+   {200,300},{300,300},{400,300},{500,300},{600,300},
+   {250,375},{350,375},{450,375},{550,375},
+   {300,450},{400,450},{500,450}}--x,y --]]
 local terrainDistribution = {{"wood",4},{"sheep",4},{"wheat",4},{"brick",3},{"stone",3},{"desert",1}}
 local devCardDistribution = {{"knight",14},{"VP",5},{"cornucopia",2},{"top-hat",2},{"road",2}}
 local resourceDistribution = {{"wood",19},{"sheep",19},{"wheat",19},{"brick",19},{"stone",19}}
@@ -34,16 +34,16 @@ local harborDistribution = {{"wood",1},{"sheep",1},{"wheat",1},{"brick",1},{"sto
 local buildingDistribution = {{"path",15},{"settlement",5},{"city",4}}
 local numberMapping = {5,2,6,3,8,10,9,12,11,4,8,10,9,4,5,6,3,11}
 function unpackDistribution(distribution, shuffle)
-  unpackedDistribution = {}
-  for _, item in ipairs(distribution) do
-    for i = 1, item[2] do
-      table.insert(unpackedDistribution,item[1])
-    end
-  end
-  if shuffle == true then
-    unpackedDistribution = lume.shuffle(unpackedDistribution)
-  end
-  return unpackedDistribution
+   unpackedDistribution = {}
+   for _, item in ipairs(distribution) do
+      for i = 1, item[2] do
+         table.insert(unpackedDistribution,item[1])
+      end
+   end
+   if shuffle == true then
+      unpackedDistribution = lume.shuffle(unpackedDistribution)
+   end
+   return unpackedDistribution
 end
 
 
@@ -106,13 +106,13 @@ function serverEventHandler(dt)
       if event.action == "move" then
          obj = objectById(event.id)
          if obj then
-           obj.x = event.x
-           obj.y = event.y
-           obj.rotation = event.rotation
-           obj.flipped = event.flipped
-           lume.remove(gameObjects, obj)
-           lume.push(gameObjects, obj)
-           net.updatePosition(obj)
+            obj.x = event.x
+            obj.y = event.y
+            obj.rotation = event.rotation
+            obj.flipped = event.flipped
+            lume.remove(gameObjects, obj)
+            lume.push(gameObjects, obj)
+            net.updatePosition(obj)
          end
       end
       if event.action == "roll" then
@@ -141,12 +141,12 @@ function clientEventHandler(dt)
       if event.action == "move" then
          local obj = objectById(event.id)
          if obj then
-           obj.x = event.x
-           obj.y = event.y
-           obj.rotation = event.rotation
-           obj.flipped = event.flipped
-           lume.remove(gameObjects, obj)
-           lume.push(gameObjects, obj)
+            obj.x = event.x
+            obj.y = event.y
+            obj.rotation = event.rotation
+            obj.flipped = event.flipped
+            lume.remove(gameObjects, obj)
+            lume.push(gameObjects, obj)
          end
       end
       if event.action == "roll" then
@@ -162,9 +162,9 @@ function love.draw()
    elseif gamemode == "foreplay" then
       for i, obj in ipairs(gameObjects) do
          if obj.pieceType == "building" then
-           love.graphics.setColor(obj.r,obj.g,obj.b)
+            love.graphics.setColor(obj.r,obj.g,obj.b)
          else
-           love.graphics.reset()
+            love.graphics.reset()
          end
          drawGrabbableObject(obj)
       end
@@ -292,67 +292,67 @@ function joinMode(hostname)
 end
 
 function initializeGameboard()
-    dice1 = newGrabbableObject("dice","white-1","white-1",25,25,50,50,true)
-    table.insert(gameObjects, dice1)
-    dice2 = newGrabbableObject("dice","black-1","black-1",75,25,50,50,true)
-    table.insert(gameObjects, dice2)
-    local terrainHexMapping = unpackDistribution(terrainDistribution,true)
-    for i, terrain in lume.ripairs(terrainHexMapping) do
-        table.insert(gameObjects, newGrabbableObject("terrainHex", terrain.."-hex", "water-hex",hexLocation[i][1],hexLocation[i][2],100,100,true))
-        if terrain ~= "desert" then
-          table.insert(gameObjects,newGrabbableObject("numberChit",numberMapping[1].."-chit", "0-chit",hexLocation[i][1],hexLocation[i][2],50,50,true,0))
-          table.remove(numberMapping,1)
-        else
-          table.insert(gameObjects,newGrabbableObject("thief","thief","thief",hexLocation[i][1],hexLocation[i][2],50,50,true))
-        end
-        table.remove(terrainHexMapping,i)
-    end
-    local harborMapping = unpackDistribution(harborDistribution,true)
-    local j = 1
-    for i = 1, 18 do
-        if i % 2 == 1 then
-          table.insert(gameObjects, newGrabbableObject("harbor", harborMapping[j].."-harbor", "water-hex", harborLocation[i][1],harborLocation[i][2],100,100,true))
-          j = j + 1
-        else
-          table.insert(gameObjects, newGrabbableObject("water", "water-hex", "water-hex", harborLocation[i][1],harborLocation[i][2],100,100,true))
-        end
-    end
-    local devCardMapping = unpackDistribution(devCardDistribution,true)
-    for i, card in lume.ripairs(devCardMapping) do
+   dice1 = newGrabbableObject("dice","white-1","white-1",25,25,50,50,true)
+   table.insert(gameObjects, dice1)
+   dice2 = newGrabbableObject("dice","black-1","black-1",75,25,50,50,true)
+   table.insert(gameObjects, dice2)
+   local terrainHexMapping = unpackDistribution(terrainDistribution,true)
+   for i, terrain in lume.ripairs(terrainHexMapping) do
+      table.insert(gameObjects, newGrabbableObject("terrainHex", terrain.."-hex", "water-hex",hexLocation[i][1],hexLocation[i][2],100,100,true))
+      if terrain ~= "desert" then
+         table.insert(gameObjects,newGrabbableObject("numberChit",numberMapping[1].."-chit", "0-chit",hexLocation[i][1],hexLocation[i][2],50,50,true,0))
+         table.remove(numberMapping,1)
+      else
+         table.insert(gameObjects,newGrabbableObject("thief","thief","thief",hexLocation[i][1],hexLocation[i][2],50,50,true))
+      end
+      table.remove(terrainHexMapping,i)
+   end
+   local harborMapping = unpackDistribution(harborDistribution,true)
+   local j = 1
+   for i = 1, 18 do
+      if i % 2 == 1 then
+         table.insert(gameObjects, newGrabbableObject("harbor", harborMapping[j].."-harbor", "water-hex", harborLocation[i][1],harborLocation[i][2],100,100,true))
+         j = j + 1
+      else
+         table.insert(gameObjects, newGrabbableObject("water", "water-hex", "water-hex", harborLocation[i][1],harborLocation[i][2],100,100,true))
+      end
+   end
+   local devCardMapping = unpackDistribution(devCardDistribution,true)
+   for i, card in lume.ripairs(devCardMapping) do
       table.insert(gameObjects, newGrabbableObject("card", "round-shield", card,750,550,100,100,true))
-    end
-    local resourceMapping = unpackDistribution(resourceDistribution,false)
-    for i, resource in lume.ripairs(resourceMapping) do
-        local ypos = 50
-        if resource == "wood" then
-            ypos = 50
-        elseif resource == "sheep" then
-          ypos = 150
-        elseif resource == "wheat" then
-          ypos = 250
-        elseif resource == "brick" then
-          ypos = 350
-        else --stone
-          ypos = 450
-        end
-        table.insert(gameObjects, newGrabbableObject("card", resource, "honeycomb",750,ypos, 100, 100, true))
-    end
-    local buildingMapping = unpackDistribution(buildingDistribution)
-    local colorMapping =  {{0/255,255/255,0/255},{255/255,165/255,0/255},{160/255,32/255,240/255},{150/255,75/255,0/255}}
-    for k = 1, 4 do
+   end
+   local resourceMapping = unpackDistribution(resourceDistribution,false)
+   for i, resource in lume.ripairs(resourceMapping) do
+      local ypos = 50
+      if resource == "wood" then
+         ypos = 50
+      elseif resource == "sheep" then
+         ypos = 150
+      elseif resource == "wheat" then
+         ypos = 250
+      elseif resource == "brick" then
+         ypos = 350
+      else --stone
+         ypos = 450
+      end
+      table.insert(gameObjects, newGrabbableObject("card", resource, "honeycomb",750,ypos, 100, 100, true))
+   end
+   local buildingMapping = unpackDistribution(buildingDistribution)
+   local colorMapping =  {{0/255,255/255,0/255},{255/255,165/255,0/255},{160/255,32/255,240/255},{150/255,75/255,0/255}}
+   for k = 1, 4 do
       local r = colorMapping[k][1]
       local g = colorMapping[k][2]
       local b = colorMapping[k][3]
       for i, building in ipairs(buildingMapping) do
-        local h = 30
-        local w = 30
-        if building == "path" then
-          w = 15
-          h = 50
-        end
-        table.insert(gameObjects, newGrabbableObject("building", building, building,200,50,w,h,true,0,r,g,b))
+         local h = 30
+         local w = 30
+         if building == "path" then
+            w = 15
+            h = 50
+         end
+         table.insert(gameObjects, newGrabbableObject("building", building, building,200,50,w,h,true,0,r,g,b))
       end
-    end
+   end
 end
 
 function newGrabbableObject (pieceType, image, back, x, y, w, h, centered, rot , r, g, b)
@@ -375,9 +375,9 @@ function newGrabbableObject (pieceType, image, back, x, y, w, h, centered, rot ,
       grabbableObject.oy = graphics[image]:getHeight()/2
    end
    if rot then
-     grabbableObject.rotation = rot
+      grabbableObject.rotation = rot
    else
-     grabbableObject.rotation = 0
+      grabbableObject.rotation = 0
    end
    grabbableObject.x = x
    grabbableObject.y = y
@@ -390,10 +390,10 @@ end
 
 function drawGrabbableObject (grabbableObject)
    if grabbableObject.flipped then
-       love.graphics.draw(graphics[grabbableObject.back], grabbableObject.x, grabbableObject.y , grabbableObject.rotation, grabbableObject.sx, grabbableObject.sy, grabbableObject.ox, grabbableObject.oy)
+      love.graphics.draw(graphics[grabbableObject.back], grabbableObject.x, grabbableObject.y , grabbableObject.rotation, grabbableObject.sx, grabbableObject.sy, grabbableObject.ox, grabbableObject.oy)
    else
       love.graphics.draw(graphics[grabbableObject.image], grabbableObject.x, grabbableObject.y , grabbableObject.rotation, grabbableObject.sx, grabbableObject.sy, grabbableObject.ox, grabbableObject.oy)
-    end
+   end
 end
 
 function aboveGrabbableObject(grabbableObject, cursorx, cursory)
@@ -401,17 +401,17 @@ function aboveGrabbableObject(grabbableObject, cursorx, cursory)
    if not specialClick and lume.find({"terrainHex", "water", "harbor","numberChit", "dice"}, grabbableObject.pieceType) then
       return false
    end
-  if grabbableObject.centered then
-    return cursorx > grabbableObject.x - grabbableObject.width/2 and
-      cursorx < grabbableObject.x + grabbableObject.width/2 and
-      cursory > grabbableObject.y - grabbableObject.height/2 and
-      cursory < grabbableObject.y + grabbableObject.height/2
-  else
-    return cursorx > grabbableObject.x and
-      cursorx < grabbableObject.x + grabbableObject.width and
-      cursory > grabbableObject.y and
-      cursory < grabbableObject.y + grabbableObject.height
-  end
+   if grabbableObject.centered then
+      return cursorx > grabbableObject.x - grabbableObject.width/2 and
+         cursorx < grabbableObject.x + grabbableObject.width/2 and
+         cursory > grabbableObject.y - grabbableObject.height/2 and
+         cursory < grabbableObject.y + grabbableObject.height/2
+   else
+      return cursorx > grabbableObject.x and
+         cursorx < grabbableObject.x + grabbableObject.width and
+         cursory > grabbableObject.y and
+         cursory < grabbableObject.y + grabbableObject.height
+   end
 end
 
 function moveGrabbableObject(grabbableObject, x, y)
@@ -420,10 +420,10 @@ function moveGrabbableObject(grabbableObject, x, y)
 end
 
 function rollDice()
-  local dicePossibilities = {1,2,3,4,5,6}
-  local diceRoll1 = lume.randomchoice(dicePossibilities)
-  local diceRoll2 = lume.randomchoice(dicePossibilities)
-  dice1.image = "white-"..diceRoll1
-  dice2.image = "black-"..diceRoll2
-  net.rollDice(diceRoll1, diceRoll2)
+   local dicePossibilities = {1,2,3,4,5,6}
+   local diceRoll1 = lume.randomchoice(dicePossibilities)
+   local diceRoll2 = lume.randomchoice(dicePossibilities)
+   dice1.image = "white-"..diceRoll1
+   dice2.image = "black-"..diceRoll2
+   net.rollDice(diceRoll1, diceRoll2)
 end
